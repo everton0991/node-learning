1eecc0b8cd6ff427eede9286223b3365a18e159f
Everton Renan <everton.renan.pc@gmail.com>
2021-09-16 10:01:58 -0300

    test: add jest configuration scripts

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,13 @@ module.exports = {
   roots: ['<rootDir>/src'],
   testEnvironment: 'node',
   coverageDirectory: 'coverage',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
