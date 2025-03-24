06a9216c63644f4c362d6e3b9182ed1129d3c4c8
Everton Renan <everton.renan@clevertech.biz>
2025-03-24 16:52:44 -0300

    test: ensure BcryptAdapter returns true when compare succeeds

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -52,4 +52,11 @@ describe('Bcrypt Adapter', () => {
     await sut.compare('any_value', 'any_hash')
     expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
   })
+
+  test('Should return a true when compare succeeds', async () => {
+    const sut = makeSut()
+    const compare = await sut.compare('any_value', 'any_hash')
+
+    expect(compare).toBe(true)
+  })
 })
